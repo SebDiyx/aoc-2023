25d35bd0d06c7cb5db3b4afea6c9d5d03842db99
sebastiandixon <sebastian.dixon@glxdigital.com>
2023-12-11 19:17:10 +0800

    Day 5 b ???

--- 5/b.ts
+++ 5/b.ts
@@ -1,108 +1,50 @@
 import fs from 'fs';
 
 const input = await fs.readFileSync('./input.txt', 'utf-8');
+const [seedsLine, ...blocks] = input.split('\n\n');
 
-const seedRanges: [number, number][] = 123;
+let seedRanges: [number, number][] = seedsLine
+    .replace('seeds: ', '')
+    .match(/(\d+) (\d+)/g)!
+    .map((range) => {
+        const [start, numSeeds] = range.split(' ').map((num) => parseInt(num));
+        return [start, start + numSeeds];
+    });
 
-// Dest Start, Source Start, Range
-const seedSoilMap: [number, number, number][] = [];
-const soilToFertilizerMap: [number, number, number][] = [];
-const fertilizerToWaterMap: [number, number, number][] = [];
-const waterToLightMap: [number, number, number][] = [];
-const LightToTemperatureMap: [number, number, number][] = [];
-const temperatureToHumidityMap: [number, number, number][] = [];
-const humidityToLocationMap: [number, number, number][] = [];
+for (const block of blocks) {
+    const ranges = block
+        .split('\n')
+        .slice(1)
+        .map((line) => line.split(' ').map((num) => parseInt(num)))
+        .sort((a, b) => a[0] - b[0]);
 
-const nameToMap = {
-    'seed-to-soil': seedSoilMap,
-    'soil-to-fertilizer': soilToFertilizerMap,
-    'fertilizer-to-water': fertilizerToWaterMap,
-    'water-to-light': waterToLightMap,
-    'light-to-temperature': LightToTemperatureMap,
-    'temperature-to-humidity': temperatureToHumidityMap,
-    'humidity-to-location': humidityToLocationMap,
-} as const;
-
-const mapsOrder = [
-    seedSoilMap,
-    soilToFertilizerMap,
-    fertilizerToWaterMap,
-    waterToLightMap,
-    LightToTemperatureMap,
-    temperatureToHumidityMap,
-    humidityToLocationMap,
-];
-
-// Populate our maps
-for (const lineGroup of input.split('\n\n')) {
-    // Get seeds from our input
-    if (lineGroup.startsWith('seeds: ')) {
-        const ranges = lineGroup.replace('seeds: ', '').match(/(\d+) (\d+)/g);
-
-        ranges?.forEach((range, i) => {
-            const [start, numSeeds] = range.split(' ').map((num) => parseInt(num));
-
-            if (i === 0) {
-                seedRanges.push([start, start + numSeeds]);
-            } else {
-                const prevRange = seedRanges[i - 1];
-                const [prevStart, prevEnd] = prevRange;
-
-                if (start < prevEnd) {
-                    seedRanges.push([prevEnd, start + numSeeds]);
-                } else {
-                    seedRanges.push([start, start + numSeeds]);
+    const mappedRanges: [number, number][] = [];
+    while (seedRanges.length > 0) {
+        const [start, end] = seedRanges.pop()!;
+        let addedToMappedRanges = false;
+        for (const [destStart, sourceStart, range] of ranges) {
+            const overlapStart = Math.max(start, sourceStart);
+            const overlapEnd = Math.min(end, sourceStart + range);
+            if (overlapStart < overlapEnd) {
+                mappedRanges.push([
+                    overlapStart - sourceStart + destStart,
+                    overlapEnd - sourceStart + destStart,
+                ]);
+                if (overlapStart > start) {
+                    seedRanges.push([start, overlapStart]);
                 }
+                if (end > overlapEnd) {
+                    seedRanges.push([overlapEnd, end]);
+                }
+                addedToMappedRanges = true;
+                break;
             }
-        });
-    }
-
-    // Get our maps from our input
-    if (lineGroup.includes('map:')) {
-        const [mapName, ...mapLines] = lineGroup.split('\n');
-        const map = nameToMap[mapName.replace(' map:', '')];
-
-        for (const mapLine of mapLines) {
-            const [destStart, sourceStart, range] = mapLine.split(' ').map((num) => parseInt(num));
-            map.push([destStart, sourceStart, range]);
         }
-        map.sort((a, b) => {
-            return a[1] - b[1];
-        });
+        if (!addedToMappedRanges) {
+            mappedRanges.push([start, end]);
+        }
     }
+    seedRanges = mappedRanges;
 }
 
-// let seeds = new Set<number>();
-// for (const seedRange of seedRanges) {
-//     const [seedStart, numSeeds] = seedRange;
-//     for (let seed = seedStart; seed < seedStart + numSeeds; seed++) {
-//         seeds.add(seed);
-//     }
-// }
-
-// console.log(seeds);
-console.log(seedRanges);
-let closestLocation: number | null = null;
-for (const range of seedRanges) {
-    console.log(range);
-    for (let seed = range[0]; seed < range[1]; seed++) {
-        let val = seed;
-        for (const map of mapsOrder) {
-            let tempVal = val;
-            for (const [destStart, sourceStart, range] of map) {
-                if (val >= sourceStart && val <= sourceStart + range) {
-                    const offset = val - sourceStart;
-                    tempVal = destStart + offset;
-                }
-            }
-            val = tempVal;
-        }
-        if (!closestLocation) {
-            closestLocation = val;
-        } else if (val < closestLocation) {
-            closestLocation = val;
-        }
-    }
-}
-
-console.log('closest', closestLocation);
+console.log(Math.min(...seedRanges.map(([a, b]) => Math.min(a, b))));
